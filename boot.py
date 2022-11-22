94389ac622e92090c1d2c9d8b08b31b51ab0cda7
MirakelDev <118356358+MirakelDev@users.noreply.github.com>
2022-11-22 11:41:43 +0100

    - mount storage as NOT readonly (needed to write log files)
    - add logger to boot.py

--- boot.py
+++ boot.py
@@ -7,9 +7,15 @@ import usb_midi
 from digitalio import Pull
 
 import safe_mode
+from logger import Logger
 from portal import Portal
 
 safe_mode.enable(board.GP14, Pull.UP)
+
+storage.remount("/", False)
+
+logger = Logger('boot.py', '/log.txt')
+logger.log(Logger.INFO, "running boot.py")
 
 #storage.disable_usb_drive()
 usb_cdc.disable()
